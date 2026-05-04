1071674c2f22f355b10c3181d516848108c22b68
krishna Sharma <krishnasharma02309@gmail.com>
2026-05-04 11:17:21 +0530

    postman test

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
-
 router.route("/register").post(registerUser)
 
 export default router
